6dc49eae5e1c4b11456ad2f207125d282d867d48
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-17 20:50:32 +0800

    Update Circle.java

--- src/Entity/Circle.java
+++ src/Entity/Circle.java
@@ -1,1 +1,29 @@
+package Entity;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import Main.Display;
+
+public class Circle extends Entity {
+	
+	private Color color=Color.BLACK;
+	
+	public Circle(Display display,int x,int y,int width,int height) {
+		super(display,x,y,width,height);
+	}
+	
+	public void setColor(Color color) {
+		this.color=color;
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render(Graphics2D g) {
+		g.setColor(color);
+		g.drawOval(x,y,width,height);
+	}
+	
+}
