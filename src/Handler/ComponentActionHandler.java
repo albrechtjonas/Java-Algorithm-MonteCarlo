e4375b539a45c78d6255ff458bc0d45efc730bd7
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-11-05 20:41:59 -0800

    Update ComponentActionHandler.java

--- src/Handler/ComponentActionHandler.java
+++ src/Handler/ComponentActionHandler.java
@@ -1,1 +1,33 @@
+package Handler;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+import Main.Display;
+
+public class ComponentActionHandler implements ActionListener {
+	
+	private Display display;
+	
+	public ComponentActionHandler(Display display) {
+		this.display=display;
+	}
+	
+	public void actionPerformed(ActionEvent actionEvent) {
+		JTextField textField=display.getWindow().getTextField();
+		
+		if(textField.getText().equals("solve")) {
+			display.getRunState().startRandomSolving();
+			display.getWindow().getTextField().setText("");
+		}else if(textField.getText().equals("reset")) {
+			display.getRunState().reset();
+			display.getWindow().getTextField().setText("");
+		}else {
+			JOptionPane.showMessageDialog(display.getWindow().getFrame(),"Unknown Command");
+			display.getWindow().getTextField().setText("");
+		}
+	}
+}
