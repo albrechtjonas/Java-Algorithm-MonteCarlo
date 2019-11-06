ea14c79d19ca2ab69f689416afe5e5c024f8f750
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-11-05 20:43:12 -0800

    Update MonteCarlo.java

--- src/Main/MonteCarlo.java
+++ src/Main/MonteCarlo.java
@@ -1,1 +1,26 @@
+package Main;
 
+public class MonteCarlo {
+	
+	private final String title="Monte Carlo";
+	
+	private final int width=801;
+	
+	private final int height=801;
+	
+	public Window window;
+	
+	private Display display;
+	
+	public static void main(String[]args) {
+		new MonteCarlo();
+	}
+	
+	private MonteCarlo() {
+		window=new Window(title,width,height);
+		
+		display=new Display(window);
+		
+		display.start();
+	}
+}
